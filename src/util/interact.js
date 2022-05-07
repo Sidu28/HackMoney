7c63feea2aca792ae1b7d5af213e04b7e1bc122d
smudgil <38195323+smudgil@users.noreply.github.com>
2022-05-07 12:45:45 -0400

    ooops

--- src/util/interact.js
+++ src/util/interact.js
@@ -4,9 +4,8 @@ import axios from "axios";
 const { ethers } = require("ethers");
 
 export const getContractABI = async (address, network) => {
-  console.log("what")
+
   let req = `https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.REACT_APP_ETHERSCAN_KEY}`;
-  console.log(req)
 
   if (network !== "mainnet") {
     req = `https://api-${network}.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.REACT_APP_ETHERSCAN_KEY}`;
@@ -22,12 +21,14 @@ export const getABIFunctions = (abi) => {
 };
 
 export const initContract = async (address, abi, network) => {
+
+
   const mapping = {
-    "mainnet": "MCkqdWaXvnh1tne6CEG6NXYQEhgA_B9Q",
-    "rinkeby" : "ElybgFn6H7hx1EfEsGvMKsD-psxb2gsy",
-    "ropsten" : "Zrit8evhI8bUxgmCIe9Yuw1nE28LK4-Z",
-    "goerli" : "qkOPUKaZ4CT4gtQSuJP5KS8NVsLxVnHO",
-    "kovan": "rif7Mgg8iXcTt54n4AG7ZfneepsWPtdn"
+    "mainnet": process.env.REACT_APP_ALCHEMY_MAINNET,
+    "rinkeby" : process.env.REACT_APP_ALCHEMY_RINKEBY,
+    "ropsten" : process.env.REACT_APP_ALCHEMY_ROPSTEN,
+    "goerli" : process.env.REACT_APP_ALCHEMY_GOERLI,
+    "kovan": process.env.REACT_APP_ALCHEMY_KOVAN
   }
   const key = mapping[network];
   const alchemy = `https://eth-${network}.alchemyapi.io/v2/${key}`;
@@ -35,6 +36,20 @@ export const initContract = async (address, abi, network) => {
   const customProvider = new ethers.providers.JsonRpcProvider(alchemy);
   const contract = new ethers.Contract(address, abi, customProvider);
   return contract;
+
+  // const mapping = {
+  //   "mainnet": "MCkqdWaXvnh1tne6CEG6NXYQEhgA_B9Q",
+  //   "rinkeby" : "ElybgFn6H7hx1EfEsGvMKsD-psxb2gsy",
+  //   "ropsten" : "Zrit8evhI8bUxgmCIe9Yuw1nE28LK4-Z",
+  //   "goerli" : "qkOPUKaZ4CT4gtQSuJP5KS8NVsLxVnHO",
+  //   "kovan": "rif7Mgg8iXcTt54n4AG7ZfneepsWPtdn"
+  // }
+  // const key = mapping[network];
+  // const alchemy = `https://eth-${network}.alchemyapi.io/v2/${key}`;
+  // console.log(alchemy)
+  // const customProvider = new ethers.providers.JsonRpcProvider(alchemy);
+  // const contract = new ethers.Contract(address, abi, customProvider);
+  // return contract;
 };
 
 export const parseInputs = (inputs, ogInputs) => {
